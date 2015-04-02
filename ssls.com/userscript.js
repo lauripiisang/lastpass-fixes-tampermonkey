c912d55719063d265af39c934a07e891ffa006b7
lauripiisang <noskiller@gmail.com>
2015-04-02 15:12:31 +0100

    Fixed URLs

--- ssls.com/userscript.js
+++ ssls.com/userscript.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Lastpass fix SSLs.com
 // @namespace    http://github.com/lauripiisang/lastpass-fixes-tampermonkey/ssls.com
-// @downloadURL https://raw.githubusercontent.com/lauripiisang/lastpass-fixes-tampermonkey/ssls.com/userscript.js
-// @updateURL https://raw.githubusercontent.com/lauripiisang/astpass-fixes-tampermonkey/ssls.com/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/lauripiisang/lastpass-fixes-tampermonkey/master/ssls.com/userscript.js
+// @updateURL    https://raw.githubusercontent.com/lauripiisang/lastpass-fixes-tampermonkey/master/ssls.com/userscript.js
 // @version      0.1.1
 // @description  removes bullshit element that blocks lastpass
 // @author       Lauri Piisang
