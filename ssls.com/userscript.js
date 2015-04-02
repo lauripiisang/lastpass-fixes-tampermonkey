2d5bc9a0f3850aacde15f8728f4b91c7c81ef528
lauripiisang <noskiller@gmail.com>
2015-04-02 15:10:19 +0100

    Added SSLs.com bullshit element remover

---
+++ ssls.com/userscript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Lastpass fix SSLs.com
+// @namespace    http://github.com/lauripiisang/lastpass-fixes-tampermonkey/ssls.com
+// @downloadURL https://raw.githubusercontent.com/lauripiisang/lastpass-fixes-tampermonkey/ssls.com/userscript.js
+// @updateURL https://raw.githubusercontent.com/lauripiisang/astpass-fixes-tampermonkey/ssls.com/userscript.js
+// @version      0.1.1
+// @description  removes bullshit element that blocks lastpass
+// @author       Lauri Piisang
+// @match        https://www.ssls.com/login-page.htm*
+// @grant        none
+// ==/UserScript==
+
+var bsel = document.getElementsByClassName("recPassHideCheckBox"); 
+if(bsel.length)
+  bsel[0].remove() ;
